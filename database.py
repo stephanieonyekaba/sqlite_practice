86e217f9890ca9747b120c072d3476f8af98d745
stephanieonyekaba <onyek009@umn.edu>
2023-01-17 21:44:56 -0600

    We ordered our records by desc, last name, first name, etc.

--- database.py
+++ database.py
@@ -5,12 +5,8 @@ connection = sqlite3.connect('customer.db')
 
 cursor = connection.cursor()
 
-# this is how we delete a record by its rowid (primary key)
-
-cursor.execute("""DELETE from customers WHERE rowid = 2 """)
-
-# querying our database
-cursor.execute("""SELECT rowid, * FROM customers""")
+# this is how we order a record. DESC means descending
+cursor.execute("""SELECT rowid, * FROM customers ORDER BY rowid DESC""")
 
 items = cursor.fetchall()
 for item in items:
