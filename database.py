d2926b320147f48ea830dcc5224a6cb9122b67bb
stephanieonyekaba <onyek009@umn.edu>
2023-01-17 20:09:58 -0600

    we used a for loop to print out each of our records in our table. we also created a variable for the table

--- database.py
+++ database.py
@@ -8,17 +8,11 @@ cursor = connection.cursor()
 # here we are running a query to show everything in our customers database
 cursor.execute("SELECT * FROM customers")
 
-# we can then run several different commands
-# connection.fetcone() --> this will display the last record added to our database
-# connection.fetchmany(2) --> this will fetch two records in our database
-# connection.fetchall() --> this will fetch all the records in our database
+items = cursor.fetchall()
 
-# we wrap our command in a print statement so we can see it in the terminal
+for item in items:
+    print(item)
 
-# this will feth the first record in our table and return the item in the 2nd index
-# of that record(email address)
-
-print(cursor.fetchone()[2])
 
 # print("COMMAND EXECUTED CORRECTLY")
 
