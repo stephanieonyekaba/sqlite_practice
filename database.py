e9488a9e5a25a79c33178951e575de12f4ed496e
stephanieonyekaba <onyek009@umn.edu>
2023-01-17 20:07:07 -0600

    we use the fetchone command followed by an index in brackets to fetch the item in the specified index for that record

--- database.py
+++ database.py
@@ -15,7 +15,10 @@ cursor.execute("SELECT * FROM customers")
 
 # we wrap our command in a print statement so we can see it in the terminal
 
-print(cursor.fetchall())
+# this will feth the first record in our table and return the item in the 2nd index
+# of that record(email address)
+
+print(cursor.fetchone()[2])
 
 # print("COMMAND EXECUTED CORRECTLY")
 
