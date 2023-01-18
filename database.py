104f92f0fa864cd32cc28dd3a676668748e9ee34
stephanieonyekaba <onyek009@umn.edu>
2023-01-17 21:00:50 -0600

    updating a record based on its primary key (row id)

--- database.py
+++ database.py
@@ -5,15 +5,16 @@ connection = sqlite3.connect('customer.db')
 
 cursor = connection.cursor()
 
-# this is how we update a record
+# this is how we update a record by its rowid (primary key)
 cursor.execute(
-    """UPDATE customers SET first_name = 'Alex' WHERE last_name = 'Anderson'  """)
+    """UPDATE customers SET first_name = 'girly' WHERE rowid = 7  """)
 
 # here we query the database
-cursor.execute("""SELECT * FROM customers """)
+cursor.execute("""SELECT rowid, * FROM customers """)
 
 items = cursor.fetchall()
-print(items)
+for item in items:
+    print(item)
 
 
 # print("COMMAND EXECUTED CORRECTLY")
