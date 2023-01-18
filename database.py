4f38bfe0e67ae2ede67f6418a5f501ca09761898
stephanieonyekaba <onyek009@umn.edu>
2023-01-17 21:57:27 -0600

    we use the limit tool to limit the number of records we fetch

--- database.py
+++ database.py
@@ -5,9 +5,9 @@ connection = sqlite3.connect('customer.db')
 
 cursor = connection.cursor()
 
-# this is how we query our database using AND/OR clause
+# this is how we query our database to limit the number of records we get
 cursor.execute(
-    """SELECT rowid, * FROM customers WHERE last_name = 'Onyekaba' OR rowid = 3 """)
+    """SELECT rowid, * FROM customers WHERE last_name = 'Onyekaba' ORDER BY rowid DESC LIMIT 2 """)
 
 items = cursor.fetchall()
 for item in items:
