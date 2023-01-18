4d21881ae99aa7f7b76e462d16843e86fe87a49f
stephanieonyekaba <onyek009@umn.edu>
2023-01-17 18:47:12 -0600

    Here we inserted many records into our database at the same time. We did this by creating a variable and passing it into our executemany command

--- database.py
+++ database.py
@@ -1,25 +1,27 @@
 import sqlite3
 
-# in order for us to do anything we need to create a connection to a database we will create.
-# we pass in the name of the database in the parentheses
 
 connection = sqlite3.connect('customer.db')
 
-# we create a cursor so we can make a table
-# we establish a connection
 cursor = connection.cursor()
 
-# we are just running sql commands now to insert a record into our customers database. remember that
-# sql commands are always capitalized
-cursor.execute(
-    "INSERT INTO customers VALUES('Mich', 'Onyekaba', 'm.o@gmail.com')")
+# to insert many records at the same time we create a variable called many_customer
+# in the dictionary many_customer, we can create several tupals (objects)
+many_customer = [
+    ('Pj', 'Watson', 'pw@gmail.com'),
+    ('Tim', 'Jones', 'bb@gmail.com'),
+    ('Sam', 'Anderson', 'hotgirl101@gmail.com'),
+]
 
-# here i just wrote a print statement to show that teh command was
-# executed correctly each time we run python3 database.py (remember database.py is the file name)
+# we run the variable executemany and use the sql ? syntax
+# we then pass in our variable many_customer
+cursor.executemany(
+    "INSERT INTO customers VALUES(?, ?, ?)", many_customer)
+
+
 print("COMMAND EXECUTED CORRECTLY")
 
-# here we are commiting so the table 'customers' is pushed into our database 'connection'
 connection.commit()
 
-# this command just closes our connection after we commit it
+
 connection.close()
